52bf7e9cb215182bd686f97aa0a08a710db72177
Won Rhim <wwrk22@gmail.com>
2023-01-25 19:40:21 -0500

    Add a route for redirecting signout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "events#index"
 
   resources :events
+
+  # Devise User
+  devise_scope :user do
+    get 'users', to: 'devise/sessions#new'
+  end
+  devise_for :users
 end
