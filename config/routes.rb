4414079eacadc9fab28b02c12cf4410f282abb36
Won Rhim <wwrk22@gmail.com>
2023-01-25 21:43:23 -0500

    Generate SessionsCtlr to customize actions

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
 
   # Devise User
   devise_scope :user do
-    get 'users', to: 'devise/sessions#new'
+    
   end
-  devise_for :users
+
+  devise_for :users, controllers: { sessions: 'users/sessions' }
 end
