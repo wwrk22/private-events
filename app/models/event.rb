48894f78dac99acf70317039d475e8eff7978e2f
Won Rhim <wwrk22@gmail.com>
2023-01-25 20:24:43 -0500

    Add the association of a User hosting an Event - A single Event only has one host (a User). - A single User (a host) can host multiple Events.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+  # An Event is hosted by a User.
+  belongs_to :host, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # A User can be the host of an event.
+  has_many :hosted_events, foreign_key: :host_id, class_name: 'Event'
+
 end
