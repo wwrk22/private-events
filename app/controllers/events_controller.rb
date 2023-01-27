d21f3cb711df6c4bbd34a95c265a3cf741a7017e
Won Rhim <wwrk22@gmail.com>
2023-01-27 16:08:21 -0500

    Add attendance ctlr

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
   before_action :set_event, only: [:show]
 
   def index
-    @events = Event.all
+    @events = Event.all.includes(:host)
   end
 
   def new
