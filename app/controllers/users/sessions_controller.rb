0d1db4f3b54fc2e886e67a6483eb18df4cad0956
Won Rhim <wwrk22@gmail.com>
2023-01-27 22:16:09 -0500

    Replace unused class name association with the correct alias

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -11,8 +11,11 @@ class Users::SessionsController < Devise::SessionsController
       Time.zone = 'Eastern Time (US & Canada)'
 
       # This won't execute a query since the associated :hosted_events has already been loaded into memory.
-      # In #set_user, #includes calls #preload to query the :hosted_events belonging to @user.
+      # In #set_user, the method #includes calls #preload to query the :hosted_events belonging to @user.
       @hosted_events = @user.hosted_events
+
+      # Get all the attendance records for the current user in order to list all the attended events.
+      @attendances = Attendance.where('attendances.attendant_id' => current_user.id).includes(:attended_event)
     else # User is not signed in.
       # For now, just redirect to avoid crashing.
       render :index, status: :ok
